e1e79879405c2a07fdbb4494039c2bd94cd280bc
ereticor <ereticor@gmail.com>
2021-03-10 23:53:20 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) == false || array.length == 0) {
+    return 0;
+  }
+  else {
+    return array.sort(function (a, b) {
+      return a - b;
+    })[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) == false || array.length == 0) {
+    return 0;
+  }
+  else {
+    return array.sort(function (a, b) {
+      return a - b;
+    })[array.length - 1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) == false || array.length == 0) {
+    return 0;
+  }
+  else {
+    let sum = 0;
+    for (i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  }
 }
+// maybe it's not a clever solution but i coded it myself
